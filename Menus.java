d8b45df9bddf3039ed7956ba83af1a2dd7b2a10c
miguel reano <161211887+miguelreano@users.noreply.github.com>
2024-02-26 00:13:16 +0800

    Add files via upload

---
+++ Menus.java
@@ -0,0 +1,146 @@
+import java.util.Scanner;
+
+public class Menus {
+
+    public Menus(){
+        TitleScreen();
+    }
+
+    public static void TitleScreen() {
+        Scanner scanner = new Scanner(System.in);
+
+      // Title Screen
+      System.out.println("\n=== Title Screen ===");
+      System.out.println("[1] Start");
+      System.out.println("[2] Exit");
+
+      System.out.print("\nEnter your choice: ");
+      int titleOption = scanner.nextInt();
+
+      switch (titleOption) {
+        case 1:
+        GameLobby();
+        case 2:
+        System.exit(0);
+        default:
+        System.out.println("Invalid choice. Please try again.");
+      }
+    }
+
+    public static void GameLobby(){
+        Scanner scanner = new Scanner(System.in);
+
+      // Character Creation
+      System.out.println("[1] Fast Travel");
+      System.out.println("[2] Level Up");
+      System.out.println("[3] Inventory");
+      System.out.println("[4] Shop");
+      System.out.println("[5] Quit Game");
+
+      System.out.print("\nEnter your choice: ");
+      int option = scanner.nextInt();
+
+      switch (option) {
+        case 1:
+        FastTravel();
+        case 2:
+        LevelUp();
+        case 3:
+        System.out.println("Inventory");
+        case 4:
+        System.out.println("Shop");
+        case 5:
+        TitleScreen();
+        default:
+        System.out.println("Invalid choice. Please try again.");
+      }
+    }
+
+    public static void CharacterCreation(){
+        Scanner scanner = new Scanner(System.in);
+
+      // Character Creation
+      System.out.println("[1] Input Name");
+      System.out.println("[2] Select Job Class");
+      System.out.println("[3] Confirm");
+      System.out.println("[4] Back");
+
+      System.out.print("\nEnter your choice: ");
+      int option = scanner.nextInt();
+
+      switch (option) {
+        case 1:
+        System.out.println("Input name");
+        case 2:
+        System.out.println("Select Job Class");
+        case 3:
+        System.out.println("Confirm");
+        case 4:
+        TitleScreen();
+        default:
+        System.out.println("Invalid choice. Please try again.");
+      }
+    }
+
+    public static void FastTravel(){
+        Scanner scanner = new Scanner(System.in);
+
+      // Character Creation
+      System.out.println("[1] Stormveil Castle");
+      System.out.println("[2] Raya Lucaria Academey");
+      System.out.println("[3] The Elden Throne");
+      System.out.println("[4] Back");
+
+      System.out.print("\nEnter your choice: ");
+      int option = scanner.nextInt();
+
+      switch (option) {
+        case 1:
+        System.out.println("Go A1");
+        case 2:
+        System.out.println("Go A2");
+        case 3:
+        System.out.println("Go A3");
+        case 4:
+        GameLobby();
+        default:
+        System.out.println("Invalid choice. Please try again.");
+      }
+    }
+
+    public static void LevelUp(){
+        Scanner scanner = new Scanner(System.in);
+
+      // Character Creation
+      System.out.println("[1] Level Health");
+      System.out.println("[2] Level Endurance");
+      System.out.println("[3] Level Dexterity");
+      System.out.println("[4] Level Strength");
+      System.out.println("[5] Level Intelligence");
+      System.out.println("[6] Level Faith");
+      System.out.println("[7] Back");
+
+      System.out.print("\nEnter your choice: ");
+      int option = scanner.nextInt();
+
+      switch (option) {
+        case 1:
+        System.out.println("Leveled HP");
+        case 2:
+        System.out.println("Leveled END");
+        case 3:
+        System.out.println("Leveled DEX");
+        case 4:
+        System.out.println("Leveled STR");
+        case 5:
+        System.out.println("Leveled INT");
+        case 6:
+        System.out.println("Leveled FTH");
+        case 7:
+        GameLobby();
+        default:
+        System.out.println("Invalid choice. Please try again.");
+      }
+    }
+    
+  }
---
+++ jobClass.java
@@ -0,0 +1,57 @@
+/*
+public class CharacterCreation {
+
+  class Vagabond(){
+    int HP = 15;
+    int END = 11;
+    int DEX = 13;
+    int STR = 14;
+    int INT = 9;
+    int FTH = 9;
+  }
+
+  class Samurai(){
+    int HP = 12;
+    int END = 13;
+    int DEX = 15;
+    int STR = 12;
+    int INT = 9;
+    int FTH = 8;
+  }
+
+  class Warrior(){
+    int HP = 11;
+    int END = 11;
+    int DEX = 16;
+    int STR = 10;
+    int INT = 10;
+    int FTH = 8;
+  }
+
+  class Hero(){
+    int HP = 14;
+    int END = 12;
+    int DEX = 9;
+    int STR = 16;
+    int INT = 7;
+    int FTH = 8;
+  }
+
+  class Astrologer(){
+    int HP = 9;
+    int END = 9;
+    int DEX = 12;
+    int STR = 8;
+    int INT = 16;
+    int FTH = 7;
+  }
+
+  class Prophet(){
+    int HP = 10;
+    int END = 8;
+    int DEX = 10;
+    int STR = 11;
+    int INT = 7;
+    int FTH = 16;
+  }
+}*/
